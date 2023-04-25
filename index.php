c602227b5ed84d864e40b2bd6ce5416b55767db0
Gabriel <117838407+TheRock-8819@users.noreply.github.com>
2023-04-25 09:25:53 -0300

    de

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$name = "gabriel";  //string
+$quantity= 14;      //integer
+$price= 9.99;       //float
+$paymentStatus= true; //bool
+$startDate= null;   //null  
+
+
+var_dump ($name);
+// echo "hola". $name;
+$taskCompleted = false;
+if ($taskCompleted){
+    echo 'Tarea Completa';
+} else {
+    'Tarea Incompleta';
+}
+
+$price= 10.50;
+if ($price <=10){
+    echo 'Comprar';
+} else{
+    echo 'No Comprar';
+}
+
+//arrays grupo de datos
+$names=[
+    'luis',
+    'jose',
+    'domenica',
+    'chelsea',
+];
+var_dump($names); //asignamos una variable
